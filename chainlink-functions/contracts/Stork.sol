73502097ea24bb6446ed55f123b995b6aae0a840
arkoc <aram.koch@gmail.com>
2023-02-26 15:21:23 -0700

    Add initial Stork contract boilplate

---
+++ chainlink-functions/contracts/Stork.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./dev/functions/FunctionsClient.sol";
+// import "@chainlink/contracts/src/v0.8/dev/functions/FunctionsClient.sol"; // Once published
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/**
+ * @title Stork contract
+ * @notice This contract is used for allowing users to send assets to twitter handles, then those users can claim those assets back
+ */
+contract Stork is FunctionsClient, ConfirmedOwner {
+  using Functions for Functions.Request;
+
+  bytes32 public latestRequestId;
+  bytes public latestResponse;
+  bytes public latestError;
+
+  event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
+
+  /**
+   * @notice Executes once when a contract is created to initialize state variables
+   *
+   * @param oracle - The FunctionsOracle contract
+   */
+  constructor(address oracle) FunctionsClient(oracle) ConfirmedOwner(msg.sender) {}
+
+  /**
+   * @notice Send a simple request
+   * 
+   * @param source JavaScript source code
+   * @param secrets Encrypted secrets payload
+   * @param args List of arguments accessible from within the source code
+   * @param subscriptionId Billing ID
+   */
+  function executeRequest(
+    string calldata source,
+    bytes calldata secrets,
+    Functions.Location secretsLocation,
+    string[] calldata args,
+    uint64 subscriptionId,
+    uint32 gasLimit
+  ) public onlyOwner returns (bytes32) {
+    Functions.Request memory req;
+    req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, source);
+    if (secrets.length > 0) {
+      if (secretsLocation == Functions.Location.Inline) {
+        req.addInlineSecrets(secrets);
+      } else {
+        req.addRemoteSecrets(secrets);
+      }
+    }
+    if (args.length > 0) req.addArgs(args);
+
+    bytes32 assignedReqID = sendRequest(req, subscriptionId, gasLimit);
+    latestRequestId = assignedReqID;
+    return assignedReqID;
+  }
+
+  /**
+   * @notice Callback that is invoked once the DON has resolved the request or hit an error
+   *
+   * @param requestId The request ID, returned by sendRequest()
+   * @param response Aggregated response from the user code
+   * @param err Aggregated error from the user code or from the execution pipeline
+   * Either response or error parameter will be set, but never both
+   */
+  function fulfillRequest(
+    bytes32 requestId,
+    bytes memory response,
+    bytes memory err
+  ) internal override {
+    latestResponse = response;
+    latestError = err;
+    emit OCRResponse(requestId, response, err);
+  }
+
+  /**
+   * @notice Allows the Functions oracle address to be updated
+   *
+   * @param oracle New oracle address
+   */
+  function updateOracleAddress(address oracle) public onlyOwner {
+    setOracle(oracle);
+  }
+
+  function addSimulatedRequestId(address oracleAddress, bytes32 requestId) public onlyOwner {
+    addExternalRequest(oracleAddress, requestId);
+  }
+
+
+  function sendToTwitter(string calldata handle) public {
+
+  }
+
+  function balanceOfTwitter(string calldata handle) public view {
+
+  }
+
+  function prepareClaim(
+    bytes calldata encryptedAccessToken,
+    string calldata destinationAddress) public returns (bytes32) {
+        Functions.Request memory req;
+        req.initializeRequest(Functions.Location.Inline, Functions.CodeLanguage.JavaScript, "todo_source");
+        if (encryptedAccessToken.length > 0) {
+            req.addInlineSecrets(encryptedAccessToken);
+        }
+
+        string[] memory args = new string[](1);
+        args[0] = destinationAddress;
+        req.addArgs(args);
+
+        bytes32 assignedReqID = sendRequest(req, 159, 100000);
+        latestRequestId = assignedReqID;
+        return assignedReqID;
+    }
+  
+  function Claim() public {
+    
+  }
+
+}
