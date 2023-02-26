30ddfe8f9d13f85f757335a5ea4f96f618aafb00
arkoc <aram.koch@gmail.com>
2023-02-26 14:45:51 -0700

    Update function to use oauth2 token

--- chainlink-functions/stork-twitter.js
+++ chainlink-functions/stork-twitter.js
@@ -1,23 +1,18 @@
-// Get the arguments from the request config
 const twitterAccessToken = secrets.twitterAccessToken;
 const ethereumAddress = args[0];
 
-// Don't even try if the username or address is empty
 if (!twitterAccessToken || !ethereumAddress) {
   throw Error('Twitter username or Ethereum address is empty');
 }
 
-// Prepare the API requests
 const twitterRequest = {
-    // Get the user id from the provided username
     identityByAccessToken: () =>
       Functions.makeHttpRequest({
-        url: 'https://api.twitter.com/1.1/account/verify_credentials.json',
+        url: 'https://api.twitter.com/2/users/me',
         headers: { Authorization: `Bearer ${twitterAccessToken}` },
       }),
   };
 
-// Request twitter handle
 const handleRes = await new Promise((resolve, reject) => {
     twitterRequest.identityByAccessToken().then((res) => {
       if (!res.error) {
@@ -32,14 +27,12 @@ const handleRes = await new Promise((resolve, reject) => {
     throw Error('Twitter API request failed - coult not get user id');
   }
 
-// Grab the tw itter handle
-const twitterHandle = handleRes.data.screen_name || null;
+const twitterHandle = handleRes.data.data.username || null;
 
-// Let's be extra careful and make sure that tweeter handle is not null
 if (!twitterHandle) {
   throw Error('Twitter API request failed - user id is null');
 }
 
 return Functions.encodeString(
-    `${twitterUsername}|${ethereumAddress}`,
+    `${twitterHandle}|${ethereumAddress}`,
   );
