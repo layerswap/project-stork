e971be176de20b0285c2adae6fefe71ae5998c09
arkoc <aram.koch@gmail.com>
2023-02-26 18:51:10 -0700

    Add deployment script for Stork contract

--- chainlink-functions/tasks/Functions-client/deployClient.js
+++ chainlink-functions/tasks/Functions-client/deployClient.js
@@ -50,3 +50,51 @@ task("functions-deploy-client", "Deploys the FunctionsConsumer contract")
 
     console.log(`\nFunctionsConsumer contract deployed to ${clientContract.address} on ${network.name}`)
   })
+
+  task("functions-deploy-stork", "Deploys the Stork contract")
+  .addOptionalParam("verify", "Set to true to verify client contract", false, types.boolean)
+  .setAction(async (taskArgs) => {
+    if (network.name === "hardhat") {
+      throw Error(
+        'This command cannot be used on a local hardhat chain.  Specify a valid network or simulate an FunctionsConsumer request locally with "npx hardhat functions-simulate".'
+      )
+    }
+
+    console.log(`Deploying Stork contract to ${network.name}`)
+
+    const oracleAddress = networkConfig[network.name]["functionsOracleProxy"]
+
+    console.log("\n__Compiling Contracts__")
+    await run("compile")
+
+    const clientContractFactory = await ethers.getContractFactory("Stork")
+    const clientContract = await clientContractFactory.deploy(oracleAddress)
+
+    console.log(
+      `\nWaiting ${VERIFICATION_BLOCK_CONFIRMATIONS} blocks for transaction ${clientContract.deployTransaction.hash} to be confirmed...`
+    )
+    await clientContract.deployTransaction.wait(VERIFICATION_BLOCK_CONFIRMATIONS)
+
+    const verifyContract = taskArgs.verify
+
+    if (verifyContract) {
+      try {
+        console.log("\nVerifying contract...")
+        await clientContract.deployTransaction.wait(Math.max(6 - VERIFICATION_BLOCK_CONFIRMATIONS, 0))
+        await run("verify:verify", {
+          address: clientContract.address,
+          constructorArguments: [oracleAddress],
+        })
+        console.log("Contract verified")
+      } catch (error) {
+        if (!error.message.includes("Already Verified")) {
+          console.log("Error verifying contract.  Try delete the ./build folder and try again.")
+          console.log(error)
+        } else {
+          console.log("Contract already verified")
+        }
+      }
+    } 
+
+    console.log(`\Stork contract deployed to ${clientContract.address} on ${network.name}`)
+  })
