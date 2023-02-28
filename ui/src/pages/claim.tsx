044c4646180dc5f3736a2a0581059eae26009154
babgev <babgev@gmail.com>
2023-02-28 00:14:53 -0700

    Use typescript magic to combine two write calls

--- ui/src/pages/claim.tsx
+++ ui/src/pages/claim.tsx
@@ -30,23 +30,21 @@ let prepareClaimTwitterHandle = (handleOfAddressData: string | undefined, userDa
 
 function prepareClaim(userData: { token: string; userName: string; }, handleOfAddressData: string | undefined): ReturnType<typeof prepareClaimTwitterHandle | typeof prepareClaimFunds> {
     // If twitter account not associated, associate the handle and claim funds in one tx
-    if (handleOfAddressData !== userData?.userName)
-    {
+    if (handleOfAddressData !== userData?.userName) {
         return prepareClaimTwitterHandle(handleOfAddressData, userData);
     }
     // If Twitter handle is already associated with the account, just call claim
-    else{
+    else {
         return prepareClaimFunds(handleOfAddressData, userData);
     }
 }
 
-function writeClaim(prepare: ReturnType<typeof prepareClaim>)
-{
+function writeClaim(prepare: ReturnType<typeof prepareClaim>) {
     // Shitty trick, think of a better one
-    if (prepare.config.functionName == 'claimFunds'){
+    if (prepare.config.functionName == 'claimFunds') {
         return useContractWrite(prepare.config);
     }
-    else{
+    else {
         return useContractWrite(prepare.config);
     }
 }
@@ -66,68 +64,22 @@ export default function Claim() {
         enabled: isConnected && Boolean(address)
     });
 
-    // If Twitter handle is already associated with the account, just call claim
-    const {
-        config: prepareClaimFundsConfig,
-        error: prepareClaimFundsError,
-        isError: isPrepareClaimFundsError
-    } = usePrepareContractWrite({
-        address: STORK_CONTRACT_ADDRESS,
-        abi: storkABI,
-        functionName: 'claimFunds',
-        enabled: Boolean(userData?.token) && handleOfAddressData == userData?.userName,
-        overrides: {
-            gasLimit: BigNumber.from(1500000),
-            type: 0
-        }
+    const claimPrepare = prepareClaim(userData, handleOfAddressData);
+    const { data: claimResult, write: callClaim, isError: isClaimCallError, error: claimCallError } = writeClaim(claimPrepare);
+    const { isLoading: isClaimLoading, isSuccess: isClaimSuccess } = useWaitForTransaction({
+        hash: claimResult?.hash,
     });
 
-    const { data: writeClaimFundsData, write: writeClaimFunds, error: writeClaimFundsError, isError: isWriteClaimFundsError } = useContractWrite(prepareClaimFundsConfig)
-    const { isLoading: isClaimFundsLoading, isSuccess: isClaimFundsSuccess } = useWaitForTransaction({
-        hash: writeClaimFundsData?.hash,
-    });
-
-    // If twitter account not associated, associate the handle and claim funds in one tx
-    const {
-        config: prepareClaimHandleConfig,
-        error: prepareClaimHandleError,
-        isError: isPrepareClaimHandleError
-    } = usePrepareContractWrite({
-        address: STORK_CONTRACT_ADDRESS,
-        abi: storkABI,
-        functionName: 'claimTwitterHandle',
-        args: [userData?.userName, userData?.token, true],
-        enabled: Boolean(userData?.token) && handleOfAddressData !== userData?.userName,
-        overrides: {
-            gasLimit: BigNumber.from(1500000),
-            type: 0
-        }
-    });
-
-    const prepareClaimC =  prepareClaim(userData, handleOfAddressData);
-    const { data: writeData, write, isError, error }= writeClaim(prepareClaimC);
-    const { isLoading, isSuccess } = useWaitForTransaction({
-        hash: writeData?.hash,
-    });
-    
-    const { data: writeClaimHandleData, write: writeClaimHandle, error: writeClaimHandleError, isError: isWriteClaimHandleError } = useContractWrite(prepareClaimHandleConfig)
-    const { isLoading: isClaimHandleLoading, isSuccess: isClaimHandleSuccess } = useWaitForTransaction({
-        hash: writeClaimHandleData?.hash,
-    });
-
-    let isClaimSuccess = (isClaimHandleSuccess || isClaimFundsSuccess);
-    let isClaimLoading = (isClaimHandleLoading || isClaimFundsLoading);
-    let claimTxHash = writeClaimHandleData?.hash || writeClaimFundsData?.hash;
-    let isClaimError = isPrepareClaimHandleError || isWriteClaimHandleError || isPrepareClaimFundsError || isWriteClaimFundsError;
-    let claimError = prepareClaimHandleError || writeClaimHandleError || prepareClaimFundsError || writeClaimFundsError;
-    let canClaim = (Boolean(writeClaimFunds) || Boolean(writeClaimHandle)) && !isClaimLoading;
+    let isClaimError = claimPrepare.isError || isClaimCallError;
+    let claimError = claimPrepare.error || claimCallError;
+    let canClaim = Boolean(claimResult) && !isClaimLoading;
 
     return (
         <>
             <div>
                 <p>  Yo {userData.userName} token is {userData.token};</p>
-                {isWriteClaimHandleError &&
-                    `Couldn't fetch balance because of ${writeClaimHandleError}`
+                {readError &&
+                    `Couldn't fetch balance because of ${readError}`
                 }
                 {data &&
                     `Your balance is ${data}`
@@ -140,20 +92,14 @@ export default function Claim() {
                 }
                 {isConnected &&
                     <button disabled={!canClaim} onClick={() => {
-                        if (handleOfAddressData !== userData?.userName)
-                        {
-                            writeClaimHandle?.();
-                        }
-                        else {
-                            writeClaimFunds?.();
-                        }
+                        callClaim?.();
                     }}>{isClaimLoading ? 'Claiming...' : 'Claim'}</button>
                 }
                 {isClaimSuccess && (
                     <div>
                         Successfully claimed!
                         <div>
-                            <a href={`${polygonMumbai.blockExplorers.etherscan.url}/tx/${claimTxHash}`}>Explorer</a>
+                            <a href={`${polygonMumbai.blockExplorers.etherscan.url}/tx/${claimResult}`}>Explorer</a>
                         </div>
                     </div>
                 )}
