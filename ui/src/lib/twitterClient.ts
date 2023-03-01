7a96d4c8c0a1062a3a36be95a409e5857047a889
babgev <babgev@gmail.com>
2023-02-28 19:26:24 -0700

    Fix vercel url resolution: hopefully

--- ui/src/lib/twitterClient.ts
+++ ui/src/lib/twitterClient.ts
@@ -1,10 +1,11 @@
 import { Client, auth } from "twitter-api-sdk";
 import { TWITTER_CLIENT_ID } from "./constants";
 
-let homeUrl = process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://www.localhost:3000';
+let vercelURL = process.env.VERCEL_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
+let homeURL = vercelURL ? 'https://' + vercelURL : 'http://localhost3000'   
 let authClient = new auth.OAuth2User({
     client_id: TWITTER_CLIENT_ID as string,
-    callback: `${homeUrl}/salon`,
+    callback: `${homeURL}/salon`,
     scopes: ["tweet.read", "users.read"],
 });
 
