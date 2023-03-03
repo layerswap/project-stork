06fd94cd3bd03659c1127cf87ab946a646876bc9
babgev <babgev@gmail.com>
2023-03-02 20:44:53 -0700

    Fixed undefined handle being passed as query param

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -40,7 +40,8 @@ export default function Home() {
 
                   <form onSubmit={(e) => {
                     e.preventDefault();
-                    router.push(`/send?handle=${handle}`)
+                    let url = '/send' + (handle != undefined ? `?handle=${handle}` : '');
+                    router.push(url);
                   }} method="POST" className="mt-8 mb-2 sm:mt-10">
                     <div className="relative p-2 sm:border sm:border-gray-400 group sm:rounded-xl sm:focus-within:ring-1 sm:focus-within:ring-gray-900 sm:focus-within:border-gray-900">
                       <input
